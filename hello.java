44eb27415aff38cf89cc3d5a4cb5fd50dd3de752
Alex <xiongdruid@gmail.com>
2021-07-21 22:42:38 +0500

    new test file

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
-class hello{
-    public static void main(
-            String args[]){
-     System.out.println("Hello world, I am java");
-     System.out.println("This is my first java program");
+public class hello{
+    public static void main(String[] args) {
+         System.out.println("Hello world, I am java");
+         System.out.println("This is my first java program");
     }  
 } 
