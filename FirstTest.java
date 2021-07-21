4b51a35e365a1a6947ee5e4aa62bcc8eb867caf4
Alex <xiongdruid@gmail.com>
2021-07-21 23:27:13 +0500

    new test file

--- FirstTest.java
+++ FirstTest.java
@@ -1,6 +1,7 @@
 public class FirstTest {
-    public static void main(String args){
+    public static void main(String[] args){
         int a = 3;
+        a++;
         System.out.println("a = " + a);
     }
 }
